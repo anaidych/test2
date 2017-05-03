61b4e4a11667a18b42d442db030d8c694b55dee9
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-05-03 18:03:34 +0300

    Modified 4 items, New 4 items

--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.formlargecaptivating', {
+        views: {
+            app: {
+                controller: 'app_formlargecaptivating',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.formlargecaptivating');
+                }
+            }
+        }
     });
 });
