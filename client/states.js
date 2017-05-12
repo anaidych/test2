f765b5b86c6d9a1b1919001208db5f7efbfa760b
test <qqq@q>
2017-05-12 16:24:38 +0300

    [Studio] Delete screen "app.formlargeclassic", Delete screen model "formlargeclassic"

--- client/states.js
+++ client/states.js
@@ -33,14 +33,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.formlargeclassic', {
-        views: {
-            app: {
-                controller: 'app_formlargeclassic',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.formlargeclassic');
-                }
-            }
-        }
     });
 });
